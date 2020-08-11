91934eed3645865874783f9a613798a86fdcff16
supply <supply@192.168.24.23>
2020-08-11 14:35:34 +0900

    9割基礎完成、残金持越し問題

--- 自販機演習課題/src/自販機演習課題/Main2.java
+++ 自販機演習課題/src/自販機演習課題/Main2.java
@@ -25,6 +25,7 @@ public class Main2 {
 		detail.add("w‘å’n‚ÌŒb‚İ‚ğ‘Ì‚Éx");
 		detail.add("w‚³‚í‚â‚©‚Åƒtƒ‹[ƒeƒB[‚È–¡‚í‚¢x");
 		
+		int deposit = 0;
 		int first =0;
 		int second =0;
 		//true‚ÈŒÀ‚èƒ‹[ƒv
@@ -34,7 +35,7 @@ public class Main2 {
 		System.out.println("]]]]]");
 		System.out.println("1:‹àŠz“ü—Í");
 		System.out.println("2:¤•i‘I‘ğ");
-		System.out.println("9:–ß‚é");
+		System.out.println("9:I—¹‚·‚é");
 		System.out.println("]]]]]");
 		System.out.println("”Ô†‚ğ‘I‘ğ‚µ‚Ä‚­‚¾‚³‚¢");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -48,14 +49,14 @@ public class Main2 {
 		
 			switch (first) {
 			case 9:
-				check = false;
+				check = false;				
 				break;
 				
 			case 1 :
 //				check = true;
 //				break;
 				 // w“üÅ’á‹àŠz‚Ìê‡A’Ç‰Á“ü‹à
-		        int deposit = 0;
+		       // int deposit = 0;
 		        int minSaleAmount = price.get(0);
 		        Scanner scanner = new Scanner(System.in);
 		        do {
@@ -72,22 +73,89 @@ public class Main2 {
 //		                loopCount++;
 //		            }
 		        } while(deposit < minSaleAmount);
-		        for (int i=0; i<name.size(); i++) {
+		        if (100<= deposit && deposit <200) { 
+		        	System.out.println("|1|…|100‰~|");
+		        	System.out.println("¤•i”Ô†‚ğ‰Ÿ‚µ‚Ä‚­‚¾‚³‚¢");
+		        	 Scanner scan = new Scanner(System.in); 
+				        this.number = scan.nextInt(); 
+		        	switch (number) {
+		        	//…‚ª‘I‘ğ‚³‚ê‚½‚Æ‚«
+		        	case 1 :
+		        		System.out.println("…‚Å‚·"); 
+		        		// ‚¨‚Â‚è
+			        	int i = 0;
+			            deposit = deposit - price.get(i);
+			            System.out.println("‚¨‚Â‚è‚ÍA" + deposit + "‰~‚Å‚·B");
+			        	break;
+			        
+			        default :
+			        	System.out.println("³‚µ‚¢”š‚ğ“ü—Í‚µ‚Ä‰º‚³‚¢");
+						break;
+		        	}
+		        	
+		        }
+		        
+		        if(200<=deposit) {
+		        	for (int i=0; i<name.size(); i++) {
 					System.out.println("|"+(i+1)+"|"+name.get(i)+"|"+price.get(i)+"‰~|");
 						}
-		        System.out.println("¤•i”Ô†‚ğ‰Ÿ‚µ‚Ä‚­‚¾‚³‚¢");
-		        Scanner scan = new Scanner(System.in); 
-		        this.number = scan.nextInt(); 
-		        switch (number) {
-		        case 1 :
-		        	System.out.println("…‚Å‚·"); 	
-		        	break;
-		        case 2 :
-		        	System.out.println("ƒR[ƒq[‚Å‚·");
-		        	break;
+		        	System.out.println("¤•i”Ô†‚ğ‰Ÿ‚µ‚Ä‚­‚¾‚³‚¢");
+			        Scanner scan = new Scanner(System.in); 
+			        this.number = scan.nextInt(); 
+			        switch (number) {
+			        case 1 :
+			        	System.out.println("…‚Å‚·"); 	
+			        	// ‚¨‚Â‚è
+			        	int i = 0;
+			            deposit = deposit - price.get(i);
+			            System.out.println("‚¨‚Â‚è‚ÍA" + deposit + "‰~‚Å‚·B");
+			        	break;
+			        case 2 :
+			        	System.out.println("ƒR[ƒq[‚Å‚·");
+			        	// ‚¨‚Â‚è
+			        	int i1 = 1;
+			            deposit = deposit - price.get(i1);
+			            System.out.println("‚¨‚Â‚è‚ÍA" + deposit + "‰~‚Å‚·B");
+			        	break;
+			        
+			        default: 		        	
+			            System.out.println("1~2‚Ì”š‚ğ“ü—Í‚µ‚Ä‰º‚³‚¢");
+			        
+			            break;
+			        }
 		        }
-		        check = true;
-				break;
+		      //¤•iŠJ••
+				System.out.println("E¤•iŠJ••");
+				System.out.println("]]]]]");
+				System.out.println(name.get(number-1));
+				System.out.println("|1|¤•i‚ÌÚ×‚ğ•\¦|");
+				System.out.println("|2|ŠJ••‚·‚é|");
+				System.out.println("|3|ŠJ••‚µ‚È‚¢|");
+				System.out.println("]]]]]");
+				BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+				try {
+				String a =br1.readLine();
+				 second = Integer.valueOf(a);
+				}catch(IOException e) {
+					e.printStackTrace();
+				}
+				
+				switch(second) {
+					case 1:
+					System.out.println(detail.get(number-1));	
+					break;
+					
+					case 2:
+					System.out.println("wƒvƒVƒ…[x");
+					break;
+					
+					case 3:
+					System.out.println("wŠJ‚¯‚é‚Ì‚ğ‚â‚ß‚Ü‚·x");
+				}
+			       check = true;
+			       
+		    break;
+		        
 			case 2 :
 				boolean kakunin = true;
 				while(kakunin) {
@@ -106,29 +174,32 @@ public class Main2 {
 			        
 			        switch(number){
 			            case 1:
-			            	int nyukina =0;
 			            	Scanner scannera = new Scanner(System.in);
 			            	
 			            	 do {
 			                     // “ü‹àˆ—
 			                     System.out.println("‚¨‹à‚ğ“ü‚ê‚Ä‰º‚³‚¢B");
-			                    nyukina = nyukina + scannera.nextInt();
-
+			                    deposit = deposit + scannera.nextInt();
+			                 
+			                 } while(deposit < 100);
+			            	// ‚¨‚Â‚è
+					            deposit = deposit - price.get(0);
+					            System.out.println("‚¨‚Â‚è‚ÍA" + deposit + "‰~‚Å‚·B");
 			                     
-			                 } while(nyukina < 100);
 			            	 kakunin =false;
 			                break;
-			            case 2:
-			            	int nyukinb =0;
+			            case 2:			            
 			            	Scanner scannerb = new Scanner(System.in);
 			            	
 			            	 do {
 			                     // “ü‹àˆ—
 			                     System.out.println("‚¨‹à‚ğ“ü‚ê‚Ä‰º‚³‚¢B");
-			                    nyukinb = nyukinb + scannerb.nextInt();
-
+			                    deposit = deposit + scannerb.nextInt();			                 			                     
+			                 } while(deposit < 200);
+			            	// ‚¨‚Â‚è					        	
+					            deposit = deposit - price.get(1);
+					            System.out.println("‚¨‚Â‚è‚ÍA" + deposit + "‰~‚Å‚·B");
 			                     
-			                 } while(nyukinb < 200);
 			            	 kakunin =false;
 			            	 break;    
 			        
@@ -148,9 +219,9 @@ public class Main2 {
 				System.out.println("|2|ŠJ••‚·‚é|");
 				System.out.println("|3|ŠJ••‚µ‚È‚¢|");
 				System.out.println("]]]]]");
-				BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+				BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
 				try {
-				String a =br1.readLine();
+				String a =br2.readLine();
 				 second = Integer.valueOf(a);
 				}catch(IOException e) {
 					e.printStackTrace();
